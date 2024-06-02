ebfdac5a3c648858b7ff9da900706f476ba32b54
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 13:48:10 +0530

    chore: norml function

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 import React from "react";
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client'
 
 
-const Title = ()=>(<h1 id="heading">Json is html like syntax</h1>)
 
-//componet composition
-const HeadingComponent = () => (
-    <div id="container">
+const Title =  function(){
+   return (
+        <h1>Title</h1>
+    )
+}
+
+const HeadingComponent = ()=> (
+    <div id="heading">
         <Title />
-        <h1 className="heading">jsx returned</h1>
+        <h1>This is heading component</h1>
     </div>
-
 )
 
 
+const root  = ReactDOM.createRoot(document.getElementById('root'));
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeadingComponent />);
+root.render(<HeadingComponent />)
