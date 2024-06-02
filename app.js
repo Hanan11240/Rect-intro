0d1fcc730ce0809a0630929de2e7de74ca06efcb
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 14:04:49 +0530

    react element inside another react lement

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-
-const title = (<h1 id="heading">Json is html like syntax</h1>)
+const elem = <span>Element</span>
+const title = (<h1 id="heading">
+    {elem}
+    Json is html like syntax
+    </h1>)
 
 const HeadingComponent = () => (
     <div id="container">
