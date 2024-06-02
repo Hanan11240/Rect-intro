9f2e8d6e6fde1b3c25d4e49f8b6ce8920aa075d9
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 14:22:32 +0530

    call a react component

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const elem = <span>Element</span>
 
+const Title = ()=>(<h1 id="heading">Json is html like syntax</h1>)
 
+//componet composition
 const HeadingComponent = () => (
     <div id="container">
+       {Title()}
         <h1 className="heading">jsx returned</h1>
     </div>
 
 )
 
-const title = (<h1 id="heading">
-    {elem}
-    Json is html like syntax
-    {/* this component should be usedinitialized before using it  */}
-    <HeadingComponent />
-    </h1>)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(title);
+root.render(<HeadingComponent />);
 
 
