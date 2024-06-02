7747c50667f3ee6b60040c80a6b7d0a95fac9371
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 14:09:22 +0530

    component inside element

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 
 const elem = <span>Element</span>
-const title = (<h1 id="heading">
-    {elem}
-    Json is html like syntax
-    </h1>)
+
 
 const HeadingComponent = () => (
     <div id="container">
-        {title}
         <h1 className="heading">jsx returned</h1>
     </div>
 
 )
 
+const title = (<h1 id="heading">
+    {elem}
+    Json is html like syntax
+    {/* this component should be usedinitialized before using it  */}
+    <HeadingComponent />
+    </h1>)
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<HeadingComponent />);
+root.render(title);
 
 
