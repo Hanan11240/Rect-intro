abb4b9647ef8fff22f3d374b6c90f7a081f410b5
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 11:57:51 +0530

    chore: jsx is not html

--- app.js
+++ app.js
@@ -1,62 +1,11 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
-// const heading = React.createElement("h1",{
-//     id:"heading",
-// },"Aslamu alikum");
-// const  root  = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
 
-//////////////////////////////////////////////////////////////////////////////////////
-//nested structure
-/**
- * <div id= parent>
- *     <div id=children>
- *          <h1>I am h1</h1>
- *          <h2> I am h2</h2>
- *      </div>
- * </div>
- * 
- * 
- */
-// const parent = React.createElement("div", { id: 'parent' },
-//     React.createElement('div', { id: 'children' },
-//         [React.createElement("h1", {}, "Aslamu alikum, it is nested"),
+const heading  = React.createElement('h1',{id:"heading"},"This is children");
 
-//         React.createElement("h2", {}, "Aslamu alikum, it is nested sibling")
-//         ]
-//     )
-// )
-/////////////////////////////////////////////////////////////////////////////////////
-
-/**
- * <div id= parent>
- *     <div id=children>
- *          <h1>I am h1</h1>
- *          <h2> I am h2</h2>
- *      </div>
- * <div id=children2>
- *          <h1>I am h1</h1>
- *          <h2> I am h2</h2>
- *      </div>
- * </div>
- * 
- * 
- * */
-const parent = React.createElement("div", { id: 'parent' },
-    [React.createElement('div', { id: 'children' },
-        [React.createElement("h1", {}, "Aslamu alikum, it is nested"),
-
-        React.createElement("h2", {}, "Aslamu alikum, it is nested sibling")
-        ]
-    ),
-    React.createElement('div', { id: 'children2' },
-        [React.createElement("h1", {}, "Aslamu alikum, it is nested"),
-
-        React.createElement("h2", {}, "Aslamu alikum, it is nested sibling")
-        ]
-    )]
-)
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(parent);
+// JSX - Html like or Xml like syntax;
+console.log('heading',heading)
+const jsxHeading = <h1 id="heading">Json is html like syntax</h1>
+console.log('jsonHeading',jsxHeading)
+const root  = ReactDOM.createRoot(document.getElementById('root'));
+root.render(jsxHeading);
