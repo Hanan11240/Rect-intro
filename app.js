4548748eb682169ef429432eabd82c2fd1670b3f
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 14:24:19 +0530

    different ways to use react component

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const Title = ()=>(<h1 id="heading">Json is html like syntax</h1>)
 const HeadingComponent = () => (
     <div id="container">
        {Title()}
+       <Title />
+       <Title></Title>
         <h1 className="heading">jsx returned</h1>
     </div>
 
