aa21e6f14e1851220bb732980a0bd985eb8bf4a9
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 12:44:55 +0530

    chore: rect component

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 import React from "react";
 import ReactDOM from 'react-dom/client';
 
-const heading  = React.createElement('h1',{id:"heading"},"This is children");
+//Jsx ->babel transpiles it to react.createelement => reactelement->js objet -> htmlelement(render)
+const jsxHeading = (<h1 id="heading">Json is html like syntax</h1>)
 
-// JSX - Html like or Xml like syntax;
-console.log('heading',heading)
-const jsxHeading = <h1 id="heading">Json is html like syntax</h1>
-console.log('jsonHeading',jsxHeading)
-const root  = ReactDOM.createRoot(document.getElementById('root'));
-root.render(jsxHeading);
+
+// React component
+
+//React Functional component (a js function that return jsx(react element)) write componnet name in capital(first letter)
+// const HeadingComponent = ()=>{
+// return <h1>jsx returned</h1>
+// }
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading">jsx returned</h1>
+    </div>
+
+)
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeadingComponent />);
