ce0de271a9340c000d122ca1712e6b5eec18b995
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-02 10:27:55 +0530

    ignite app

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
 // const heading = React.createElement("h1",{
 //     id:"heading",
 // },"Aslamu alikum");
@@ -56,5 +58,5 @@ const parent = React.createElement("div", { id: 'parent' },
 )
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-console.log(root)
+
 root.render(parent);
