83a6763d2cc01cbb2b260144ce5ece22cd45ed24
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-06 21:41:20 +0530

    use effect and shimmer ui

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
-import resData from '../utils/mockData'
+import { useEffect, useState } from "react";
+import Shimmer from "./shimmer";
 const Body = () => {
-    const [ListOfRestaurants,setListOfRestaurants] = useState(resData);
-        
+    const [ListOfRestaurants,setListOfRestaurants] = useState([]);
+       
+    useEffect(()=>{
+       fetchData();
+    },[])
+
+    const fetchData = async()=>{
+        const data  = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+        const json = await data.json();
+
+        const list  = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        setListOfRestaurants(list);
+    }
+
+    if(ListOfRestaurants.length ===0){
+        return <Shimmer />
+    }
     return (
         <div className="body">
             <div className="filter">
@@ -11,8 +26,7 @@ const Body = () => {
                     onClick={()=>{
                         const filteredList = ListOfRestaurants.filter((restaurant)=> restaurant?.info?.avgRating > 4);
                        setListOfRestaurants(filteredList);
-                       console.log(filteredList)
-
+                       
                     }}
                 >Top rated button</button>
             </div>
