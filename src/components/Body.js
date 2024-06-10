100591b58539b7a07fbe3414cf2e355c52dd4413
Hanan112401 <118827480+Hanan112401@users.noreply.github.com>
2024-06-10 14:31:49 +0530

    useEfect

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,36 +2,47 @@ import RestaurantCard from "./RestaurantCard";
 import { useEffect, useState } from "react";
 import Shimmer from "./shimmer";
 const Body = () => {
-    const [ListOfRestaurants,setListOfRestaurants] = useState([]);
-       
-    useEffect(()=>{
-       fetchData();
-    },[])
+    const [ListOfRestaurants, setListOfRestaurants] = useState([]);
+    const [filteredRestaurants,setFilteredRestaurants] = useState([]);
+    const [searchText, setSearchText] = useState("");
+    useEffect(() => {
+        fetchData();
+    }, [])
 
-    const fetchData = async()=>{
-        const data  = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+    const fetchData = async () => {
+        const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
         const json = await data.json();
 
-        const list  = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
+        const list = json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
         setListOfRestaurants(list);
+        setFilteredRestaurants(list);
     }
-
-    if(ListOfRestaurants.length ===0){
-        return <Shimmer />
-    }
-    return (
+    //conditional rendering
+    // if(ListOfRestaurants.length ===0){
+    //     return <Shimmer />
+    // }
+    return ListOfRestaurants.length === 0 ? <Shimmer /> : (
         <div className="body">
             <div className="filter">
+                <div className="search">
+                    <input type="text" className="search-box" value={searchText} onChange={(e) => {
+                            setSearchText(e?.target?.value);
+                    }} />
+                    <button onClick={() => {
+                       const filteredList = ListOfRestaurants.filter((restaurant)=>restaurant?.info?.name?.toLowerCase().includes(searchText.toLowerCase()));
+                       setFilteredRestaurants(filteredList);
+                    }}>Search</button>
+                </div>
                 <button className="filter-btn"
-                    onClick={()=>{
-                        const filteredList = ListOfRestaurants.filter((restaurant)=> restaurant?.info?.avgRating > 4);
-                       setListOfRestaurants(filteredList);
-                       
+                    onClick={() => {
+                        const filteredList = ListOfRestaurants.filter((restaurant) => restaurant?.info?.avgRating > 4);
+                        setListOfRestaurants(filteredList);
+
                     }}
                 >Top rated button</button>
             </div>
             <div className="res-container">
-                {ListOfRestaurants.map((restaurant) => (<RestaurantCard key={restaurant?.info?.id} resData={restaurant} />))}
+                {filteredRestaurants.map((restaurant) => (<RestaurantCard key={restaurant?.info?.id} resData={restaurant} />))}
             </div>
         </div>
     )
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 const HeaderComponent = () => {
+    const [buttonName,setButtonName] = useState("Login");
     return (
         <div className="header">
             <div className="logo-container">
@@ -11,6 +13,9 @@ const HeaderComponent = () => {
                     <li>About us</li>
                     <li>Contact us</li>
                     <li>Cart</li>
+                    <button onClick={()=>{
+                      buttonName === "Login" ? setButtonName('Logout'):setButtonName("Login");
+                    }} className="login">{buttonName}</button>
                 </ul>
             </div>
         </div>
